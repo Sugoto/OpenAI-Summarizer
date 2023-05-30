ecd5f1dd857309d2d4bef7bd3fc1f2a380bfe4a6
Sugoto <sugotobasu1@gmail.com>
2023-05-30 08:59:55 +0530

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Sugoto/OpenAI-Summarizer", "_blank")
           }
           className="black_btn"
         >
@@ -19,10 +19,10 @@ const Hero = () => {
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient ">OpenAI GPT-4</span>
+        <span className="orange_gradient ">OpenAI GPT Model</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with Shortenz, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
